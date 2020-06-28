99badf37ad40d94addd4a868a52f7d7292df3bb7
Swaminathan-0706 <sayeeiyer@gmail.com>
2020-06-28 20:14:21 +0530

    Functional card details

--- src/Pages/Modals/IndividualCard/IndividualCard.js
+++ src/Pages/Modals/IndividualCard/IndividualCard.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './IndividualCard.module.css';
 import TeamRetreival from '../TeamRetreival/TeamRetreival';
+import CardDetailModal from '../CardDetailModal/CardDetailModal'; 
+
 
 function IndividualCard({card,board,column}) {
+
+  const [cardDetailModal,setCardDetailModal]=useState(false);
   const members = card.teamMembers.map(name => <TeamRetreival name={name} key={name} />);
+
+
+  function openCardDetails(){
+      console.log("clicked");
+      setCardDetailModal(true);
+  }
     return (
         <>
+        {cardDetailModal?<CardDetailModal/>:
          <li
         className={styles.item}
-        
-      >
+        onClick={()=>openCardDetails()}
+        >
         <div className={styles.text}>{card.title}</div>
         <div className={styles.actions}>
           <div className={styles.actionBtn}>
@@ -23,7 +34,8 @@ function IndividualCard({card,board,column}) {
           </div>
           <div className={styles.team}>{members}</div>
         </div>
-      </li>   
+        </li>
+          } 
         </>
     )
 }
