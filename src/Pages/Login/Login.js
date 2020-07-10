b0b16ceaf98c5f41804876bb61b575fb8f3b4f81
Swaminathan-0706 <sayeeiyer@gmail.com>
2020-07-10 23:53:28 +0530

    Google Sign in

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import firebase from 'firebase';
 
 function Login() {
+
+  //Function to Google Login 
+  const googleLoginHandler=()=>{
+    const base_provider=new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(base_provider).then(function(result){
+      console.log("Success..Google Account Linked");
+    }).catch(function(error){
+      console.log(error);
+      console.log("Error");
+    })
+  }
     return (
         <form  className={styles.loginCtr}>
           <p className={styles.heading}>Login</p>
@@ -16,8 +28,17 @@ function Login() {
             <input required name="password" type="password"   placeholder="******"></input>
           </div>
           <div className={styles.group}>
-            <button type="submit">Sign Up</button>
+            <button type="submit">Login</button>
           </div>
+          <div onClick={googleLoginHandler} className={styles.google}>
+        
+        
+            <img  style={{marginTop:"7px", marginRight:"8px",width:"20px"}} alt="Google sign-in" 
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+        
+        Login with Google
+        
+      </div>
           <div className={styles.footer}>
             Don't Have An Account? 
             <Link className={styles.link} to="/signup">
